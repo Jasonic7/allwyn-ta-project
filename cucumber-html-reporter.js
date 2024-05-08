6c6f45a9d311ee38c582bc0785fdd65319542cb6
Jasonic7 <101604218+Jasonic7@users.noreply.github.com>
2024-05-08 21:05:47 +0300

    First implementation

---
+++ cucumber-html-reporter.js
@@ -0,0 +1,27 @@
+const report = require('multiple-cucumber-html-reporter');
+
+var month = (new Date).getMonth() + 1;
+var os = require('os');
+const platform = os.platform();
+const release = os.release();
+const hostnameofpc = os.hostname
+
+
+report.generate({
+	jsonDir: './jsonlogs/',
+	reportPath: './jsonlogs/',
+	pageTitle: 'Allwyn TA report',
+	reportName: 'Allwyn TA report Test Execution Report ' + (new Date).getDate() + '/' + month + '/' + (new Date).getFullYear(),
+	metadata: {
+		browser: {
+			name: 'chrome',
+			version: '110'
+		},
+		device: hostnameofpc,
+		platform: {
+			name: platform,
+			version: release,
+		},
+	},
+
+});
