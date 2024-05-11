4f3ee2af8f8596027772c315ca4cd53654d326c1
unknown <jason.tse@hotmail.com>
2024-05-11 23:35:09 +0300

    no fail on non zero exit

--- cypress/support/steps/global.step.ts
+++ cypress/support/steps/global.step.ts
@@ -1,9 +1,9 @@
 import { AfterAll, BeforeAll } from "@badeball/cypress-cucumber-preprocessor";
 
 BeforeAll(()=>{
-    cy.exec('npm run clear')
+    cy.exec('npm run clear',{failOnNonZeroExit:false})
 })
 
 AfterAll(()=>{
-    cy.exec('npm run report')
+    cy.exec('npm run report',{failOnNonZeroExit:false})
 })
